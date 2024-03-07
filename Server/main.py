e79c5f22d4a461c39091a817b875d681046e1b89
Phosphorus6 <132591588+Phosphorus6@users.noreply.github.com>
2024-03-07 12:08:07 -0600

    updated server.py

--- Server/main.py
+++ Server/main.py
@@ -5,5 +5,6 @@ import socket
 window = tk.Tk()
 window.geometry()
 window.title("Python-Messager Info")
-
-latest_comment = tk.Label()
+Main_Frame = tk.Frame(window)
+settings_frame = tk.Frame(window)
+settings_button = tk.Button(Main_Frame, text="Settings", width=10, height=3)
